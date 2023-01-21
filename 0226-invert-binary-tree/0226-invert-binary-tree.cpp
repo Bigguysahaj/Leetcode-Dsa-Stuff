803f37d9e8c37353c67e7a7437384892cb9f927d
Sahaj Singh <70798888+Bigguysahaj@users.noreply.github.com>
2023-01-21 10:55:09 +0530

    Time: 3 ms (66.10%), Space: 9.7 MB (44.59%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -18,8 +18,8 @@ public:
         TreeNode * left = invertTree(root->left);
         TreeNode * right = invertTree(root->right);
         
-        root->right = left;
         root->left = right;
+        root->right = left;
         
         return root;
     }
