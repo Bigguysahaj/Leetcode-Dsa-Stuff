3276820a34471643a2c90f9e2e47fbe989dae38e
Sahaj Singh <70798888+Bigguysahaj@users.noreply.github.com>
2023-01-25 20:38:48 +0530

    Time: 36 ms (6.46%), Space: 12.3 MB (12.74%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        unordered_map<int,int>mp;
+
+        for(auto i: nums){mp[i]++;}
+
+        while(mp.find(original)!=mp.end()){original*=2;}
+
+        return original;
+    }
+};
