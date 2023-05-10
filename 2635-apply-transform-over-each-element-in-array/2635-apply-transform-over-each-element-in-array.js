fc166a759ce17fec09ad8d08280191f9367862cc
Sahaj Singh <70798888+Bigguysahaj@users.noreply.github.com>
2023-05-11 03:27:44 +0530

    Time: 59 ms (55.28%), Space: 41.9 MB (59.42%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -3,12 +3,6 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
-    const arrN = [];
-
-    for(const elem in arr){
-        arrN.push(fn(arr[elem],Number(elem)));
-    }
-
-    return arrN;
+var map = (arr, fn) => {
+    return arr.map(fn)
 };
