957295063afad18472ed0a450761163c2b6864d4
Sahaj Singh <70798888+Bigguysahaj@users.noreply.github.com>
2023-05-17 23:43:57 +0530

    Time: 77 ms (47.12%), Space: 49.5 MB (36.02%) - LeetHub

---
+++ 2632-curry/2632-curry.ts
@@ -0,0 +1,23 @@
+function curry(fn: Function): Function {
+    
+    let nums = [];
+
+    return function curried(...args) {
+        
+        nums = [...nums, ...args];
+        
+        if(fn.length === nums.length){
+            const res = fn(...nums);
+            nums = [];
+            return res;
+        } else {
+            return curried;
+        }
+    };
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
